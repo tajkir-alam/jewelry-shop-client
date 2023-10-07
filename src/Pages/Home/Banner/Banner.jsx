d9fadb1cd849c925769a9cadbe8a892198ae5a29
tajkir-alam <tajkir.alam@gmail.com>
2023-10-07 18:43:27 +0600

    banenr added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import slider1 from '../../../assets/slider1.png';
+import slider2 from '../../../assets/slider2.png'
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay, } from "swiper";
+
+const Banner = () => {
+    return (
+        <div className='md:h-[calc(100vh-100px)]'>
+            <Swiper
+                grabCursor={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                pagination={true}
+                navigation={true}
+                modules={[Pagination, Navigation, Autoplay]}
+                className="mySwiper lg:h-full"
+            >
+                <SwiperSlide> 
+                    <span className='absolute bg-gradient-to-r from-[rgba(0,0,0,0.2)] to-[rgba(0,0,0,0.6)] w-full h-full'></span>
+                    <img src={slider1} loading="lazy" className='w-full' />
+                    <span className='text-[#ffffffe3] absolute top-1/3 left-12 w-3/4 md:top-3/4 md:left-52 text-center text-3xl lg:text-7xl z-20'>
+                        Welcome to The Jewelry Army
+                    </span>
+                </SwiperSlide>
+                <SwiperSlide> 
+                    <span className='absolute bg-gradient-to-r from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.2)] w-full h-full'></span>
+                    <img src={slider2} loading="lazy" className='w-full' />
+                    <span className='text-[#ffffffe3] absolute top-1/3 left-12 w-3/4 md:top-3/4 md:left-52 text-center text-3xl lg:text-7xl z-20'>
+                        Welcome to The Jewelry Army
+                    </span>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Banner from '../Banner/Banner';
+import Banner from '../Banner/Banner';
 // import Gallery from '../Gallery/Gallery';
 // import ShopByCategory from '../ShopByCategory/ShopByCategory';
 // import ExtraSection from '../ExtraSection/ExtraSection';
@@ -10,8 +10,8 @@ const Home = () => {
 
     return (
         <div>
-            {/* <Banner></Banner>
-            <Gallery></Gallery>
+            <Banner></Banner>
+            {/* <Gallery></Gallery>
             <ShopByCategory></ShopByCategory>
             <ExtraSection></ExtraSection> */}
         </div>
